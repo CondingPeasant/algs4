58c823722efcc52f2bb4ccfc716e4d6a559a8991
chenlili <chelili@pset.suntec.net>
2016-11-15 16:17:30 +0800

    [2] implement Subset.java
    Change-Id: I5eaa0593c1b3f9956f7ea322c0a8c6e106c0e8d8

--- DequesAndRandomizedQueues/Subset.java
+++ DequesAndRandomizedQueues/Subset.java
@@ -1,3 +1,17 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Subset {
-   public static void main(String[] args)
+   public static void main(String[] args) {
+       int k = StdIn.readInt();
+       RandomizedQueue<String> rq = new RandomizedQueue<String>();
+       String s;
+       while (null != (s = StdIn.readString())) {
+           rq.enqueue(s);
+       }
+
+       for (int i = 0; i < k; i++) {
+           StdOut.println(rq.sample());
+       }
+   }
 }
