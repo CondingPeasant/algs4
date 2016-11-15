109edf040c9150bfc5d4b62b2f0831d60c23e1e0
chenlili <chelili@pset.suntec.net>
2016-11-15 15:59:29 +0800

    add test case for RandomizedQueue
    Change-Id: I7fe7b46d1de9f9525c4c9296cd4fb797f2bb1c58

--- DequesAndRandomizedQueues/RandomizedQueue.java
+++ DequesAndRandomizedQueues/RandomizedQueue.java
@@ -1,11 +1,19 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 public class RandomizedQueue<Item> implements Iterable<Item> {
+    private static final int sizeLowBound = 1;
+    private int size;
+    private Item[] mRQ;
     // construct an empty randomized queue
     public RandomizedQueue() {
+        size = 0;
+        mRQ = (Item[]) new Object[sizeLowBound];
     }
 
     // is the queue empty?
     public boolean isEmpty() {
-        return false;
+        return true;
     }
 
     // return the number of items on the queue
@@ -15,18 +23,37 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
     // add the item
     public void enqueue(Item item) {
+        if (null == item)
+            throw new NullPointerException("Cannot add a null object.");
     }
 
     // remove and return a random item
     public Item dequeue() {
+        return null;
     }
 
     // return (but do not remove) a random item
     public Item sample() {
+        return null;
     }
+
+    private void resize() {}
 
     // return an independent iterator over items in random order
     public Iterator<Item> iterator() {
+        return new RandomizedQueueIterator();
+    }
+
+    private class RandomizedQueueIterator implements Iterator<Item> {
+        public boolean hasNext() {
+            return false;
+        }
+        public void remove() {
+            throw new UnsupportedOperationException("DequeIterator does not support remove operation.");
+        }
+        public Item next() {
+            throw new NoSuchElementException("There's no next element.");
+        }
     }
 
     // unit testing
---
+++ DequesAndRandomizedQueues/RandomizedQueueTest.java
@@ -0,0 +1,63 @@
+import static org.junit.Assert.*;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import org.junit.Test;
+
+public class RandomizedQueueTest {
+
+    @Test
+    public void testIsEmpty() {
+        RandomizedQueue<String> rq = new RandomizedQueue<String>();
+        assertTrue(rq.isEmpty());
+    }
+
+    @Test
+    public void testSize() {
+        RandomizedQueue<String> rq = new RandomizedQueue<String>();
+        assertEquals(rq.size(), 0);
+    }
+
+    @Test
+    public void testEnqueue() {
+        RandomizedQueue<String> rq = new RandomizedQueue<String>();
+        String s = null;
+        try {
+            rq.enqueue(s);
+        } catch (NullPointerException aNullPointerException) {
+        }
+    }
+
+    @Test
+    public void testDequeue() {
+        RandomizedQueue<String> rq = new RandomizedQueue<String>();
+        try {
+            rq.dequeue();
+        } catch (NoSuchElementException aNoSuchElementException) {
+        }
+    }
+
+    @Test
+    public void testSample() {
+        RandomizedQueue<String> rq = new RandomizedQueue<String>();
+        assertEquals(rq.sample(), null);
+    }
+
+    @Test
+    public void testIterator() {
+        RandomizedQueue<String> rq = new RandomizedQueue<String>();
+        Iterator<String> it = rq.iterator();
+
+        assertFalse(it.hasNext());
+        try {
+            it.remove();
+        } catch (UnsupportedOperationException aUnsupportedOperationException) {
+        }
+        try {
+            it.next();
+        } catch (NoSuchElementException aNoSuchElementException) {
+        }
+    }
+
+}
