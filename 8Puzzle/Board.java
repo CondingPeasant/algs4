eccb31bf04008ac2cdb19b14df0ecaa5acbdd9f9
chenlili <chelili@pset.suntec.net>
2016-12-01 10:16:16 +0800

    [4] amend Board.java's constructor
    Change-Id: Iec6e9f8f838359ed2d70e34a08016c635ba232b2

--- 8Puzzle/Board.java
+++ 8Puzzle/Board.java
@@ -15,10 +15,10 @@ public class Board {
         if (blocks == null)
             throw new NullPointerException();
 
-        mDimension = blocks.length - 1;
+        mDimension = blocks.length;
         mBlocks = new int[blocks.length][blocks[0].length];
-        for (int i = 1; i < blocks.length; i++) {
-            for (int j = 1; j < blocks[0].length; j++) {
+        for (int i = 0; i < blocks.length; i++) {
+            for (int j = 0; j < blocks[0].length; j++) {
                 mBlocks[i][j] = blocks[i][j];
                 if (mBlocks[i][j] == BLANK) {
                     mBlankRow = i;
@@ -36,8 +36,8 @@ public class Board {
     // number of blocks out of place
     public int hamming() {
         int count = 0;
-        for (int i = 1; i <= mDimension; i++) {
-            for (int j = 1; j <= mDimension; j++) {
+        for (int i = 0; i < mDimension; i++) {
+            for (int j = 0; j < mDimension; j++) {
                 if (!isInRightPosition(i, j) && !isBlank(i, j)) {
                     count++;
                 }
@@ -49,12 +49,12 @@ public class Board {
     // sum of Manhattan distances between blocks and goal
     public int manhattan() {
         int count = 0;
-        for (int i = 1; i <= mDimension; i++) {
-            for (int j = 1; j <= mDimension; j++) {
+        for (int i = 0; i < mDimension; i++) {
+            for (int j = 0; j < mDimension; j++) {
                 if (isInRightPosition(i, j) || isBlank(i, j))
                     continue;
-                int rightRow = (mBlocks[i][j] - 1) / mDimension + 1;
-                int rightCol = mBlocks[i][j] - mDimension * (rightRow - 1);
+                int rightRow = (mBlocks[i][j] - 1) / mDimension;
+                int rightCol = mBlocks[i][j] - mDimension * rightRow - 1;
                 count += Math.abs(rightRow - i) + Math.abs(rightCol - j);
             }
         }
@@ -63,8 +63,8 @@ public class Board {
 
     // is this board the goal board?
     public boolean isGoal() {
-        for (int i = 1; i <= mDimension; i++) {
-            for (int j = 1; j <= mDimension; j++) {
+        for (int i = 0; i < mDimension; i++) {
+            for (int j = 0; j < mDimension; j++) {
                 if (!isInRightPosition(i, j))
                     return false;
             }
@@ -77,25 +77,28 @@ public class Board {
         int[][] tmpBlocks = copyBlocks();
 
         int row1, col1, row2, col2;
-        row1 = mBlankRow + 1 <= 3 ? mBlankRow + 1 : 1;
+        row1 = (mBlankRow + 1) % mDimension;
         col1 = mBlankCol;
         row2 = mBlankRow;
-        col2 = mBlankCol + 1 <= 3 ? mBlankCol + 1 : 1;
+        col2 = (mBlankCol + 1) % mDimension;
         swapBlock(tmpBlocks, row1, col1, row2, col2);
         return new Board(tmpBlocks);
     }
 
     // does this board equal y?
     public boolean equals(Object y) {
-        if (y.getClass() != Board.class)
+        if (y == null)
+            return false;
+
+        if (this.getClass() != y.getClass())
             return false;
         Board board = (Board) y;
         
         if (mDimension != board.mDimension)
             return false;
 
-        for (int i = 1; i <= mDimension; i++) {
-            for (int j = 1; j <= mDimension; j++) {
+        for (int i = 0; i < mDimension; i++) {
+            for (int j = 0; j < mDimension; j++) {
                 if (mBlocks[i][j] != board.mBlocks[i][j])
                     return false;
             }
@@ -120,25 +123,25 @@ public class Board {
         private int capacity = 0;
 
         public BoardIterator() {
-            if (mBlankRow != 1) {
+            if (mBlankRow != 0) {
                 int[][] tmpBlocks = copyBlocks();
                 swapBlock(tmpBlocks, mBlankRow, mBlankCol,
                         mBlankRow - 1, mBlankCol);
                 mItem[capacity++] = new Board(tmpBlocks);
             }
-            if (mBlankCol != 1) {
+            if (mBlankCol != 0) {
                 int[][] tmpBlocks = copyBlocks();
                 swapBlock(tmpBlocks, mBlankRow, mBlankCol,
                         mBlankRow, mBlankCol - 1);
                 mItem[capacity++] = new Board(tmpBlocks);
             }
-            if (mBlankRow != mDimension) {
+            if (mBlankRow != mDimension - 1) {
                 int[][] tmpBlocks = copyBlocks();
                 swapBlock(tmpBlocks, mBlankRow, mBlankCol,
                         mBlankRow + 1, mBlankCol);
                 mItem[capacity++] = new Board(tmpBlocks);
             }
-            if (mBlankCol != mDimension) {
+            if (mBlankCol != mDimension - 1) {
                 int[][] tmpBlocks = copyBlocks();
                 swapBlock(tmpBlocks, mBlankRow, mBlankCol,
                         mBlankRow, mBlankCol + 1);
@@ -165,11 +168,10 @@ public class Board {
     // string representation of this board (in the output format specified below)
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        String s = String.format("%d\n", mDimension);
-        sb.append(s);
-        for (int i = 1; i <= mDimension; i++) {
-            for (int j = 1; j <= mDimension; j++)
-                sb.append(" " + mBlocks[i][j] + " ");
+        sb.append(String.format("%d\n", mDimension));
+        for (int i = 0; i < mDimension; i++) {
+            for (int j = 0; j < mDimension; j++)
+                sb.append(String.format("%2d ", mBlocks[i][j]));
             sb.append("\n");
         }
         return sb.toString();
@@ -180,12 +182,12 @@ public class Board {
     }
 
     private int convert2DTo1D(int row, int col) {
-        return (row - 1) * mDimension + col;
+        return row * mDimension + col + 1;
     }
 
     private boolean isInRightPosition(int row, int col) {
-        if (row == mDimension && col == mDimension)
-            return mBlocks[row][col] == BLANK;
+        if (mBlocks[row][col] == 0)
+            return row == mDimension - 1 && col == mDimension - 1;
         return mBlocks[row][col] == convert2DTo1D(row, col);
     }
 
@@ -201,8 +203,8 @@ public class Board {
 
     private int[][] copyBlocks() {
         int[][] tmpBlocks = new int[mBlocks.length][mBlocks[0].length];
-        for (int i = 1; i < mBlocks.length; i++) {
-            for (int j = 1; j < mBlocks[0].length; j++) {
+        for (int i = 0; i < mBlocks.length; i++) {
+            for (int j = 0; j < mBlocks[0].length; j++) {
                 tmpBlocks[i][j] = mBlocks[i][j];
             }
         }
--- 8Puzzle/BoardTest.java
+++ 8Puzzle/BoardTest.java
@@ -10,72 +10,56 @@ import org.junit.Test;
 import edu.princeton.cs.algs4.StdOut;
 
 public class BoardTest {
-    private int[][] blockGoal_2D = {{0, 0, 0},
-                                    {0, 1, 2},
-                                    {0, 3, 0}};
-    private int[][] blockGoal_3D = {{0, 0, 0, 0},
-                                    {0, 1, 2, 3},
-                                    {0, 4, 5, 6},
-                                    {0, 7, 8, 0}};
+    private int[][] blockGoal_2D = {{1, 2},
+                                    {3, 0}};
+    private int[][] blockGoal_3D = {{1, 2, 3},
+                                    {4, 5, 6},
+                                    {7, 8, 0}};
 
-    private int[][] block1 = {{0, 0, 0, 0},
-                              {0, 8, 1, 3},
-                              {0, 4, 0, 2},
-                              {0, 7, 6, 5}};
-    private int[][] block1_twin = {{0, 0, 0, 0},
-                                   {0, 8, 1, 3},
-                                   {0, 4, 0, 6},
-                                   {0, 7, 2, 5}};
-    private int[][] block1_neighbor1 = {{0, 0, 0, 0},
-                                        {0, 8, 0, 3},
-                                        {0, 4, 1, 2},
-                                        {0, 7, 6, 5}};
-    private int[][] block1_neighbor2 = {{0, 0, 0, 0},
-                                        {0, 8, 1, 3},
-                                        {0, 0, 4, 2},
-                                        {0, 7, 6, 5}};
-    private int[][] block1_neighbor3 = {{0, 0, 0, 0},
-                                        {0, 8, 1, 3},
-                                        {0, 4, 6, 2},
-                                        {0, 7, 0, 5}};
-    private int[][] block1_neighbor4 = {{0, 0, 0, 0},
-                                        {0, 8, 1, 3},
-                                        {0, 4, 2, 0},
-                                        {0, 7, 6, 5}};
+    private int[][] block1 = {{8, 1, 3},
+                              {4, 0, 2},
+                              {7, 6, 5}};
+    private int[][] block1_twin = {{8, 1, 3},
+                                   {4, 0, 6},
+                                   {7, 2, 5}};
+    private int[][] block1_neighbor1 = {{8, 0, 3},
+                                        {4, 1, 2},
+                                        {7, 6, 5}};
+    private int[][] block1_neighbor2 = {{8, 1, 3},
+                                        {0, 4, 2},
+                                        {7, 6, 5}};
+    private int[][] block1_neighbor3 = {{8, 1, 3},
+                                        {4, 6, 2},
+                                        {7, 0, 5}};
+    private int[][] block1_neighbor4 = {{8, 1, 3},
+                                        {4, 2, 0},
+                                        {7, 6, 5}};
 
-    private int[][] block2 = {{0, 0, 0, 0},
-                              {0, 8, 1, 3},
-                              {0, 4, 5, 2},
-                              {0, 7, 6, 0}};
-    private int[][] block2_twin = {{0, 0, 0, 0},
-                                   {0, 8, 1, 7},
-                                   {0, 4, 5, 2},
-                                   {0, 3, 6, 0}};
-    private int[][] block2_neighbor1 = {{0, 0, 0, 0},
-                                        {0, 8, 1, 3},
-                                        {0, 4, 5, 0},
-                                        {0, 7, 6, 2}};
-    private int[][] block2_neighbor2 = {{0, 0, 0, 0},
-                                        {0, 8, 1, 3},
-                                        {0, 4, 5, 2},
-                                        {0, 7, 0, 6}};
+    private int[][] block2 = {{8, 1, 3},
+                              {4, 5, 2},
+                              {7, 6, 0}};
+    private int[][] block2_twin = {{8, 1, 7},
+                                   {4, 5, 2},
+                                   {3, 6, 0}};
+    private int[][] block2_neighbor1 = {{8, 1, 3},
+                                        {4, 5, 0},
+                                        {7, 6, 2}};
+    private int[][] block2_neighbor2 = {{8, 1, 3},
+                                        {4, 5, 2},
+                                        {7, 0, 6}};
 
-    private int[][] block3 = {{0, 0, 0, 0},
-                              {0, 8, 0, 3},
-                              {0, 4, 5, 2},
-                              {0, 7, 6, 1}};
-    private int[][] block3_neighbor1 = {{0, 0, 0, 0},
-                                        {0, 0, 8, 3},
-                                        {0, 4, 5, 2},
-                                        {0, 7, 6, 1}};
-    private int[][] block3_neighbor2 = {{0, 0, 0, 0},
-                                        {0, 8, 5, 3},
-                                        {0, 4, 0, 2},
-                                        {0, 7, 6, 1}};
-    private int[][] block3_neighbor3 = {{0, 0, 0, 0},
-                                        {0, 8, 3, 0},
-                                        {0, 4, 5, 2},
-                                        {0, 7, 6, 1}};
+    private int[][] block3 = {{8, 0, 3},
+                              {4, 5, 2},
+                              {7, 6, 1}};
+    private int[][] block3_neighbor1 = {{0, 8, 3},
+                                        {4, 5, 2},
+                                        {7, 6, 1}};
+    private int[][] block3_neighbor2 = {{8, 5, 3},
+                                        {4, 0, 2},
+                                        {7, 6, 1}};
+    private int[][] block3_neighbor3 = {{8, 3, 0},
+                                        {4, 5, 2},
+                                        {7, 6, 1}};
     @Test
     public void testBoard() {
         try {
